8d344efc38514a74d1d2ff1ceb9bdb86c7b3de41
scarlet <sarahscarlet641@gmail.com>
2024-03-23 23:43:58 +0300

    create objects.js

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution in this file!
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+employee[key]=value;
+return employee;
+}
+const employee ={
+    name:'Scarlet',
+    role:'developer'
+};
+console.log(employee);
+
+destructivelyUpdateEmployeeWithKeyAndValue(employee,'role','Product Manager');
+console.log(employee);// after updating
+
+//sample employee object
+employee = {
+    name: 'Brian Ochieng',
+    age:30,
+    position:'Developer'
+}; 
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    return{
+        ...employee,
+        [key]:value
+    };
+}
+const updateEmployee=updateEmployeeWithKeyAndValue(employee,'position','senior manager')
+console.log(updateEmployee);
+function deleteFromEmployeeByKey(employee,key){
+    const newEmployee=Object.assign({},employee);
+    delete newEmployee[key];
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(){
+    delete employee[key];
+    return employee;
+}
+
+
